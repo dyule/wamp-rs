2241c9ee5bcc1e12238a8c0de5a0f92caccee6bb
Daniel Yule <daniel.yule@gmail.com>
2016-03-10 16:48:20 -0400

    Created serialization for Hello, Welcome, Goodbye, Abort, Error and Subscribe messages

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![cfg_attr(feature = "serde_macros", feature(custom_derive, plugin))]
+#![cfg_attr(feature = "serde_macros", plugin(serde_macros))]
+#![feature(braced_empty_structs)]
+
+extern crate serde;
+extern crate serde_json;
+
+#[cfg(feature = "serde_macros")]
+include!("main.rs.in");
+
+#[cfg(not(feature = "serde_macros"))]
+include!(concat!(env!("OUT_DIR"), "/main.rs"));
