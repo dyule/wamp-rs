e97e84ef1e5708ca3c9037996a0b59f7645e137b
Daniel Yule <daniel.yule@gmail.com>
2018-05-04 20:44:20 -0700

    Formatted with rustfmt

--- src/messages/types/mod.rs
+++ src/messages/types/mod.rs
@@ -1,32 +1,31 @@
-
 // All this is necessary because Serde decided that the best way
 // to serialize an empty struct was as a null value, and the best way
 // to deserialize it was as an empty list.  So instead we serialize it as a map.
 macro_rules! serialize_empty {
-    ($name: tt) => (
+    ($name:tt) => {
         serialize_empty_workaround!($name, $name);
-    );
+    };
 }
 
 macro_rules! serialize_empty_workaround {
-    ($name: ty, $obj: expr) => (
+    ($name:ty, $obj:expr) => {
         impl serde::Serialize for $name {
             fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-                where S: serde::Serializer,
+            where
+                S: serde::Serializer,
             {
-
                 try!(serializer.serialize_map(Some(0))).end()
             }
         }
 
-        impl <'de> serde::de::Deserialize<'de> for $name {
-
-
-            fn deserialize<D>(deserializer:  D) -> Result<$name, D::Error> where
-             D: serde::de::Deserializer<'de> {
+        impl<'de> serde::de::Deserialize<'de> for $name {
+            fn deserialize<D>(deserializer: D) -> Result<$name, D::Error>
+            where
+                D: serde::de::Deserializer<'de>,
+            {
                 {
                     struct Visitor;
-                    impl <'de> serde::de::Visitor<'de> for Visitor {
+                    impl<'de> serde::de::Visitor<'de> for Visitor {
                         type Value = $name;
 
                         fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
@@ -34,13 +33,17 @@ macro_rules! serialize_empty_workaround {
                         }
 
                         #[inline]
-                        fn visit_unit<E>(self) -> Result<$name,E> where
-                         E: serde::de::Error {
+                        fn visit_unit<E>(self) -> Result<$name, E>
+                        where
+                            E: serde::de::Error,
+                        {
                             Ok($obj)
                         }
                         #[inline]
-                        fn visit_map<A>(self,  _visitor: A) -> Result<$name,A::Error>
-                         where A: serde::de::MapAccess<'de> {
+                        fn visit_map<A>(self, _visitor: A) -> Result<$name, A::Error>
+                        where
+                            A: serde::de::MapAccess<'de>,
+                        {
                             self.visit_unit()
                         }
                     }
@@ -48,35 +51,29 @@ macro_rules! serialize_empty_workaround {
                 }
             }
         }
-    );
+    };
 }
 
-
-
-mod options;
-mod value;
 mod error;
+mod options;
 mod roles;
+mod value;
 
 use serde;
 use std::fmt;
 
-pub use messages::types::options::*;
-pub use messages::types::value::*;
 pub use messages::types::error::*;
+pub use messages::types::options::*;
 pub use messages::types::roles::*;
+pub use messages::types::value::*;
 
 fn is_not(b: &bool) -> bool {
     !*b
 }
 
-
 /**************************
          Structs
 **************************/
-
-
-
 
 /// The policies that can be used for matching a uri pattern.
 #[derive(PartialEq, Debug, Clone, Copy)]
@@ -86,7 +83,7 @@ pub enum MatchingPolicy {
     /// The given pattern contains at least one 'wildcard' segment which can match any segment at the same location
     Wildcard,
     /// The given pattern only matches URIs that are identical.
-    Strict
+    Strict,
 }
 
 /// The policies that dictate how invocations are distributed amongst shared registrations
@@ -101,9 +98,8 @@ pub enum InvocationPolicy {
     // First callee (in orer of registration) is called
     First,
     // Last callee (in order of registration( is called
-    Last
+    Last,
 }
-
 
 /**************************
         Visitors
@@ -111,10 +107,6 @@ pub enum InvocationPolicy {
 
 struct MatchingPolicyVisitor;
 struct InvocationPolicyVisitor;
-
-
-
-
 
 impl MatchingPolicy {
     #[inline]
@@ -144,34 +136,34 @@ impl Default for InvocationPolicy {
     }
 }
 
-
-
 /*-------------------------
        MatchingPolicy
 -------------------------*/
 
 impl serde::Serialize for MatchingPolicy {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: serde::Serializer,
+    where
+        S: serde::Serializer,
     {
         let ser_str = match *self {
-             MatchingPolicy::Prefix => "prefix",
-             MatchingPolicy::Wildcard => "wildcard",
-             MatchingPolicy::Strict => ""
+            MatchingPolicy::Prefix => "prefix",
+            MatchingPolicy::Wildcard => "wildcard",
+            MatchingPolicy::Strict => "",
         };
         serializer.serialize_str(ser_str)
     }
 }
 
-impl <'de> serde::Deserialize<'de> for MatchingPolicy {
+impl<'de> serde::Deserialize<'de> for MatchingPolicy {
     fn deserialize<D>(deserializer: D) -> Result<MatchingPolicy, D::Error>
-        where D: serde::Deserializer<'de>,
+    where
+        D: serde::Deserializer<'de>,
     {
         deserializer.deserialize_str(MatchingPolicyVisitor)
     }
 }
 
-impl <'de> serde::de::Visitor<'de> for MatchingPolicyVisitor {
+impl<'de> serde::de::Visitor<'de> for MatchingPolicyVisitor {
     type Value = MatchingPolicy;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
@@ -180,41 +172,46 @@ impl <'de> serde::de::Visitor<'de> for MatchingPolicyVisitor {
 
     #[inline]
     fn visit_str<E>(self, value: &str) -> Result<MatchingPolicy, E>
-        where E: serde::de::Error,
+    where
+        E: serde::de::Error,
     {
         match value {
             "prefix" => Ok(MatchingPolicy::Prefix),
             "wildcard" => Ok(MatchingPolicy::Wildcard),
-            x => Err(serde::de::Error::custom(format!("Invalid matching policy: {}", x)))
+            x => Err(serde::de::Error::custom(format!(
+                "Invalid matching policy: {}",
+                x
+            ))),
         }
     }
-
 }
 
 impl serde::Serialize for InvocationPolicy {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: serde::Serializer,
+    where
+        S: serde::Serializer,
     {
         let ser_str = match *self {
-             InvocationPolicy::Single => "single",
-             InvocationPolicy::RoundRobin => "roundrobin",
-             InvocationPolicy::Random => "random",
-             InvocationPolicy::First => "first",
-             InvocationPolicy::Last => "last",
+            InvocationPolicy::Single => "single",
+            InvocationPolicy::RoundRobin => "roundrobin",
+            InvocationPolicy::Random => "random",
+            InvocationPolicy::First => "first",
+            InvocationPolicy::Last => "last",
         };
         serializer.serialize_str(ser_str)
     }
 }
 
-impl <'de> serde::Deserialize<'de> for InvocationPolicy {
+impl<'de> serde::Deserialize<'de> for InvocationPolicy {
     fn deserialize<D>(deserializer: D) -> Result<InvocationPolicy, D::Error>
-        where D: serde::Deserializer<'de>,
+    where
+        D: serde::Deserializer<'de>,
     {
         deserializer.deserialize_str(InvocationPolicyVisitor)
     }
 }
 
-impl <'de> serde::de::Visitor<'de> for InvocationPolicyVisitor {
+impl<'de> serde::de::Visitor<'de> for InvocationPolicyVisitor {
     type Value = InvocationPolicy;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
@@ -223,7 +220,8 @@ impl <'de> serde::de::Visitor<'de> for InvocationPolicyVisitor {
 
     #[inline]
     fn visit_str<E>(self, value: &str) -> Result<InvocationPolicy, E>
-        where E: serde::de::Error,
+    where
+        E: serde::de::Error,
     {
         match value {
             "single" => Ok(InvocationPolicy::Single),
@@ -231,8 +229,10 @@ impl <'de> serde::de::Visitor<'de> for InvocationPolicyVisitor {
             "random" => Ok(InvocationPolicy::Random),
             "first" => Ok(InvocationPolicy::First),
             "last" => Ok(InvocationPolicy::Last),
-            x => Err(serde::de::Error::custom(format!("Invalid invocation policy: {}", x)))
+            x => Err(serde::de::Error::custom(format!(
+                "Invalid invocation policy: {}",
+                x
+            ))),
         }
     }
-
 }
--- src/messages/types/value.rs
+++ src/messages/types/value.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
-use CallResult;
+use super::{CallError, Reason};
 use serde;
-use super::{Reason, CallError};
+use std::collections::HashMap;
 use std::fmt;
+use CallResult;
 
 pub type Dict = HashMap<String, Value>;
 pub type List = Vec<Value>;
@@ -10,13 +10,13 @@ pub type List = Vec<Value>;
 // TODO properly implement Hash and Eq
 #[derive(Debug, PartialEq, Clone, Hash, Eq)]
 pub struct URI {
-    pub uri: String
+    pub uri: String,
 }
 
 impl URI {
     pub fn new(uri: &str) -> URI {
         URI {
-            uri: uri.to_string()
+            uri: uri.to_string(),
         }
     }
 }
@@ -31,12 +31,11 @@ pub enum Value {
     Float(f64),
     String(String),
     List(List),
-    Boolean(bool)
+    Boolean(bool),
 }
 
 struct URIVisitor;
 struct ValueVisitor;
-
 
 pub trait ArgList {
     fn get_int(&self, index: usize) -> CallResult<Option<i64>>;
@@ -57,12 +56,17 @@ impl ArgList for List {
                 if let Value::Integer(value) = *value {
                     Ok(Some(value))
                 } else {
-                    Err(CallError::new(Reason::InvalidArgument, Some(vec![Value::String(format!("Expected integer, got {}", value.summarize()))]), None))
+                    Err(CallError::new(
+                        Reason::InvalidArgument,
+                        Some(vec![Value::String(format!(
+                            "Expected integer, got {}",
+                            value.summarize()
+                        ))]),
+                        None,
+                    ))
                 }
-            },
-            None => {
-                Ok(None)
             }
+            None => Ok(None),
         }
     }
 
@@ -73,12 +77,17 @@ impl ArgList for List {
                 if let Value::String(ref value) = *value {
                     Ok(Some(value))
                 } else {
-                    Err(CallError::new(Reason::InvalidArgument, Some(vec![Value::String(format!("Expected string, got {}", value.summarize()))]), None))
+                    Err(CallError::new(
+                        Reason::InvalidArgument,
+                        Some(vec![Value::String(format!(
+                            "Expected string, got {}",
+                            value.summarize()
+                        ))]),
+                        None,
+                    ))
                 }
-            },
-            None => {
-                Ok(None)
             }
+            None => Ok(None),
         }
     }
 
@@ -86,7 +95,15 @@ impl ArgList for List {
         if self.len() >= expected_len {
             Ok(())
         } else {
-            Err(CallError::new(Reason::InvalidArgument, Some(vec![Value::String(format!("Expected {} arguments, got {}", expected_len, self.len()))]), None))
+            Err(CallError::new(
+                Reason::InvalidArgument,
+                Some(vec![Value::String(format!(
+                    "Expected {} arguments, got {}",
+                    expected_len,
+                    self.len()
+                ))]),
+                None,
+            ))
         }
     }
 }
@@ -99,12 +116,17 @@ impl ArgDict for Dict {
                 if let Value::Integer(value) = *value {
                     Ok(Some(value))
                 } else {
-                    Err(CallError::new(Reason::InvalidArgument, Some(vec![Value::String(format!("Expected integer, got {}", value.summarize()))]), None))
+                    Err(CallError::new(
+                        Reason::InvalidArgument,
+                        Some(vec![Value::String(format!(
+                            "Expected integer, got {}",
+                            value.summarize()
+                        ))]),
+                        None,
+                    ))
                 }
-            },
-            None => {
-                Ok(None)
             }
+            None => Ok(None),
         }
     }
     fn get_string<'a>(&'a self, key: &str) -> CallResult<Option<&'a str>> {
@@ -114,12 +136,17 @@ impl ArgDict for Dict {
                 if let Value::String(ref value) = *value {
                     Ok(Some(value))
                 } else {
-                    Err(CallError::new(Reason::InvalidArgument, Some(vec![Value::String(format!("Expected string, got {}", value.summarize()))]), None))
+                    Err(CallError::new(
+                        Reason::InvalidArgument,
+                        Some(vec![Value::String(format!(
+                            "Expected string, got {}",
+                            value.summarize()
+                        ))]),
+                        None,
+                    ))
                 }
-            },
-            None => {
-                Ok(None)
             }
+            None => Ok(None),
         }
     }
 }
@@ -130,44 +157,43 @@ impl Value {
             Value::Dict(ref d) => {
                 let mut result = String::new();
                 result.push('{');
-                result.push_str(&d.iter().take(50).map(|(key, value)| format!("{}:{}", key, value.summarize())).collect::<Vec<_>>().join(","));
+                result.push_str(&d.iter()
+                    .take(50)
+                    .map(|(key, value)| format!("{}:{}", key, value.summarize()))
+                    .collect::<Vec<_>>()
+                    .join(","));
                 result.push('}');
                 result
-            },
-            Value::Integer(i) => {
-                i.to_string()
-            },
-            Value::UnsignedInteger(u) => {
-                u.to_string()
-            },
-            Value::Float(f) => {
-                f.to_string()
-            },
+            }
+            Value::Integer(i) => i.to_string(),
+            Value::UnsignedInteger(u) => u.to_string(),
+            Value::Float(f) => f.to_string(),
             Value::String(ref s) => {
                 if s.len() > 50 {
                     s[..50].to_string()
                 } else {
                     s.clone()
                 }
-            },
+            }
             Value::List(ref l) => {
                 let mut result = String::new();
                 result.push('[');
-                result.push_str(&l.iter().take(50).map(|element| element.summarize()).collect::<Vec<_>>().join(","));
+                result.push_str(&l.iter()
+                    .take(50)
+                    .map(|element| element.summarize())
+                    .collect::<Vec<_>>()
+                    .join(","));
                 result.push(']');
                 result
-            },
-            Value::Boolean(b) => {
-                b.to_string()
             }
+            Value::Boolean(b) => b.to_string(),
         }
     }
 }
 
 // XXX Right now there is no way to tell the difference between a URI and a string, or an ID and an Integer
-impl <'de> serde::de::Visitor<'de> for ValueVisitor {
+impl<'de> serde::de::Visitor<'de> for ValueVisitor {
     type Value = Value;
-
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         formatter.write_str("JSON value")
@@ -175,56 +201,64 @@ impl <'de> serde::de::Visitor<'de> for ValueVisitor {
 
     #[inline]
     fn visit_str<E>(self, value: &str) -> Result<Value, E>
-        where E: serde::de::Error {
-            Ok(Value::String(value.to_string()))
+    where
+        E: serde::de::Error,
+    {
+        Ok(Value::String(value.to_string()))
     }
-
 
     #[inline]
     fn visit_i64<E>(self, value: i64) -> Result<Value, E>
-    where E: serde::de::Error {
+    where
+        E: serde::de::Error,
+    {
         Ok(Value::Integer(value))
     }
 
     #[inline]
     fn visit_u64<E>(self, value: u64) -> Result<Value, E>
-    where E: serde::de::Error {
+    where
+        E: serde::de::Error,
+    {
         Ok(Value::UnsignedInteger(value))
     }
 
     fn visit_f64<E>(self, value: f64) -> Result<Value, E>
-    where E: serde::de::Error {
+    where
+        E: serde::de::Error,
+    {
         Ok(Value::Float(value))
     }
 
     #[inline]
     fn visit_bool<E>(self, value: bool) -> Result<Value, E>
-    where E: serde::de::Error {
+    where
+        E: serde::de::Error,
+    {
         Ok(Value::Boolean(value))
     }
 
-
     #[inline]
     fn visit_map<Visitor>(self, mut visitor: Visitor) -> Result<Value, Visitor::Error>
-    where Visitor: serde::de::MapAccess<'de>,
+    where
+        Visitor: serde::de::MapAccess<'de>,
     {
-       let mut values = HashMap::new();
-       if let Some(size) = visitor.size_hint() {
-           values.reserve(size);
-       }
+        let mut values = HashMap::new();
+        if let Some(size) = visitor.size_hint() {
+            values.reserve(size);
+        }
 
-       while let Some((key, value)) = visitor.next_entry()? {
-           values.insert(key, value);
-       }
+        while let Some((key, value)) = visitor.next_entry()? {
+            values.insert(key, value);
+        }
 
+        Ok(Value::Dict(values))
+    }
 
-
-       Ok(Value::Dict(values))
-   }
-
-   #[inline]
+    #[inline]
     fn visit_seq<Visitor>(self, mut visitor: Visitor) -> Result<Value, Visitor::Error>
-        where Visitor: serde::de::SeqAccess<'de>,
+    where
+        Visitor: serde::de::SeqAccess<'de>,
     {
         let mut values = Vec::new();
         if let Some(size) = visitor.size_hint() {
@@ -235,8 +269,6 @@ impl <'de> serde::de::Visitor<'de> for ValueVisitor {
             values.push(value);
         }
 
-
-
         Ok(Value::List(values))
     }
 }
@@ -246,7 +278,8 @@ impl <'de> serde::de::Visitor<'de> for ValueVisitor {
 -------------------------*/
 impl serde::Serialize for Value {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: serde::ser::Serializer,
+    where
+        S: serde::ser::Serializer,
     {
         match *self {
             Value::Dict(ref dict) => dict.serialize(serializer),
@@ -255,14 +288,15 @@ impl serde::Serialize for Value {
             Value::UnsignedInteger(u) => serializer.serialize_u64(u),
             Value::Float(f) => serializer.serialize_f64(f),
             Value::List(ref list) => list.serialize(serializer),
-            Value::Boolean(b) => serializer.serialize_bool(b)
+            Value::Boolean(b) => serializer.serialize_bool(b),
         }
     }
 }
 
-impl <'de> serde::Deserialize<'de> for Value {
+impl<'de> serde::Deserialize<'de> for Value {
     fn deserialize<D>(deserializer: D) -> Result<Value, D::Error>
-        where D: serde::Deserializer<'de>,
+    where
+        D: serde::Deserializer<'de>,
     {
         deserializer.deserialize_any(ValueVisitor)
     }
@@ -274,33 +308,35 @@ impl <'de> serde::Deserialize<'de> for Value {
 
 impl serde::Serialize for URI {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: serde::ser::Serializer,
+    where
+        S: serde::ser::Serializer,
     {
         serializer.serialize_str(&self.uri)
     }
 }
 
-impl <'de> serde::Deserialize<'de> for URI {
+impl<'de> serde::Deserialize<'de> for URI {
     fn deserialize<D>(deserializer: D) -> Result<URI, D::Error>
-        where D: serde::Deserializer<'de>,
+    where
+        D: serde::Deserializer<'de>,
     {
         deserializer.deserialize_str(URIVisitor)
     }
 }
 
-impl <'de> serde::de::Visitor<'de> for URIVisitor {
+impl<'de> serde::de::Visitor<'de> for URIVisitor {
     type Value = URI;
 
-    fn expecting(&self,  formatter: &mut fmt::Formatter) -> fmt::Result {
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         formatter.write_str("URI")
     }
     #[inline]
     fn visit_str<E>(self, value: &str) -> Result<URI, E>
-        where E: serde::de::Error,
+    where
+        E: serde::de::Error,
     {
         Ok(URI {
-            uri: value.to_string()
+            uri: value.to_string(),
         })
     }
-
 }
